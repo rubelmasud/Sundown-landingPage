2003407b4ff808d4a71aae73745113841a6c5e35
Your Name <you@example.com>
2024-07-15 21:30:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+function loaderAnimation() {
+  var loader = document.querySelector("#loader");
+  setTimeout(function () {
+    loader.style.top = "-100%";
+  }, 4200);
+}
+
+function section3Animation() {
+  var elemC = document.querySelector("#elem-container");
+  var fixed = document.querySelector("#fixed-image");
+  elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
+  });
+  elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+  });
+
+  var elems = document.querySelectorAll(".elem");
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      fixed.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    // spaceBetween: 100,
+    autoplay: {
+      delay: 1200,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+}
+
+function menuAnimation() {
+  var menu = document.querySelector("nav h3");
+  var full = document.querySelector("#full-scr");
+  var navimg = document.querySelector("nav img");
+  var flag = 0;
+  menu.addEventListener("click", function () {
+    if (flag == 0) {
+      full.style.top = 0;
+      navimg.style.opacity = 0;
+      menu.style.opacity = 1;
+      flag = 1;
+    } else {
+      full.style.top = "-100%";
+      navimg.style.opacity = 1;
+      menu.style.opacity = 1;
+      flag = 0;
+    }
+  });
+}
+
+menuAnimation();
+swiperAnimation();
+section3Animation();
+loaderAnimation();
